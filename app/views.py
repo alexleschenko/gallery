bd586db6fb46a7ae821bd92cfe3e4d4850ac30f8
allesh93 <04111993>
2017-01-17 11:41:12 +0300

    create User Create view + template

--- app/views.py
+++ app/views.py
@@ -1,10 +1,17 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.shortcuts import render
-
+from app import forms
 # Create your views here.
 from django.views.generic import TemplateView, CreateView
 
 
 class MainPage(LoginRequiredMixin,TemplateView):
     template_name = 'main.html'
+
+class UserCreate(CreateView):
+
+    model = User
+    template_name = 'CreateUser.html'
+    form_class = forms.UserCreateForm
+    success_url = 'main'
--- gallery/settings.py
+++ gallery/settings.py
@@ -115,6 +115,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGOUT_REDIRECT_URL = '/'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
